ec4a53ed86560b74670bc2f13a1aa264b134c430
Andrii Shyian <andrew.shyian.87@gmail.com>
2019-02-27 22:14:49 +0200

    gen

--- scripts.js
+++ scripts.js
@@ -1,27 +1,66 @@
-const timeMachine = {
-    name: 'DeLorean',
-    horsepower: 200,
-    timeTravel: true,
-    maxSpeed: 160,
-    age: 36
-   }
-
-//    for(let key in timeMachine){
-//      if( typeof timeMachine[key] === 'number'){
-//          timeMachine[key] = timeMachine[key] * 2;
-//      }
-//    }
-
-//    console.log(timeMachine);
-
-function objectToArray(obj){
-const array = [];
-let i = 0;
-for(let key in obj){
-array[i] = obj[key];
-i++;
+const submit = document.getElementById('submit');
+const result = document.getElementById('result');
+let bodyParts = [];
+let adjectives = [];
+let words = [];
+function getBodyPart(element){
+   bodyParts = setData(element);
+   console.log(bodyParts);
 }
+ function getAdjectives(element){
+     adjectives = setData(element);
+     console.log(adjectives);
+ }
+ function getWords(element){
+     words = setData(element);
+     console.log(words);
+ }
+
+function setData(element){
+const array = element.value.split(',');
+if(element.value === ''){
+    element.classList.add('error');
+}
+else{
+element.classList.remove('error');
+}
+
 return array;
 }
-console.log(timeMachine);
-console.log(objectToArray(timeMachine));
+
+function randomWords(array){
+    const index = randomNumbers(array);
+    return array[index];
+}
+
+function randomNumbers(array){
+    const arraysLength = array.length - 1
+return (Math.random() * arraysLength).toFixed();
+}
+
+function generator(){
+    result.classList.add('load');
+    setTimeout(function() {
+if(bodyParts.length != 0 && adjectives.length != 0 && words.length != 0){
+result.innerHTML =  `Your ${randomWords(bodyParts)} is like a ${randomWords(adjectives)} ${randomWords(words)}`;
+}
+else{
+    console.log('Errors!');
+}
+result.classList.remove('load');
+},3000);}
+
+window.onload = function(){
+    const bodyPartsId = document.getElementById('bodyParts');
+    const adjectivesId = document.getElementById('adjectives');
+    const wordsId = document.getElementById('words');
+
+    bodyParts = bodyPartsId.value.split(',');
+    adjectives = adjectivesId.value.split(',');
+    words = wordsId.value.split(',');
+    
+
+submit.addEventListener('click', function(){
+    generator();
+});
+}
